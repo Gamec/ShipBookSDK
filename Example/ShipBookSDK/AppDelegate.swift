627cb226ac23825f34fa088c7ff9b2706085217d
elishas <7370546+elishas@users.noreply.github.com>
2026-03-01 11:17:35 +0200

    made a fully new example ui

--- Example/ShipBookSDK/AppDelegate.swift
+++ Example/ShipBookSDK/AppDelegate.swift
@@ -12,55 +12,28 @@ import ShipBookSDK
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
-    var window: UIWindow?
+  var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-      // Override point for customization after application launch.
-      ShipBook.enableInnerLog(enable: true)
-      #if compiler(>=6.1)
-      print("Hello, Swift 6.1")
-      #elseif compiler(>=6.0)
-      print("Hello, Swift 6.0")
-      #elseif compiler(>=5.10)
-      print("Hello, Swift 5.10")
-      #elseif compiler(>=5.9)
-      print("Hello, Swift 5.9")
-      #else
-      print("Hello, Swift 5.x")
-      #endif
+  func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    ShipBook.enableInnerLog(enable: true)
 
-      let info = Bundle.main.infoDictionary!
-      let appId = info["SHIPBOOK_APP_ID"] as! String
-      let appKey = info["SHIPBOOK_APP_KEY"] as! String
-      if let url = info["SHIPBOOK_URL"] as? String, !url.isEmpty {
-          ShipBook.setConnectionUrl(url)
-      }
-      ShipBook.start(appId: appId, appKey: appKey)
-      return true
-    }
-  
-    func applicationWillResignActive(_ application: UIApplication) {
-        // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
-        // Use this method to pause ongoing tasks, disable timers, and throttle down OpenGL ES frame rates. Games should use this method to pause the game.
+    let info = Bundle.main.infoDictionary!
+    let appId = info["SHIPBOOK_APP_ID"] as! String
+    let appKey = info["SHIPBOOK_APP_KEY"] as! String
+    if let url = info["SHIPBOOK_URL"] as? String, !url.isEmpty {
+      ShipBook.setConnectionUrl(url)
     }
 
-    func applicationDidEnterBackground(_ application: UIApplication) {
-        // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
-        // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
-    }
+    // Start with completion to get session URL
+    ShipBook.start(appId: appId, appKey: appKey, completion: { sessionUrl in
+      print("ShipBook session URL: \(sessionUrl)")
+    })
 
-    func applicationWillEnterForeground(_ application: UIApplication) {
-        // Called as part of the transition from the background to the inactive state; here you can undo many of the changes made on entering the background.
-    }
+    // Programmatic window setup (no storyboard)
+    window = UIWindow(frame: UIScreen.main.bounds)
+    window?.rootViewController = UINavigationController(rootViewController: ViewController())
+    window?.makeKeyAndVisible()
 
-    func applicationDidBecomeActive(_ application: UIApplication) {
-        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
-    }
-
-    func applicationWillTerminate(_ application: UIApplication) {
-        // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-
+    return true
+  }
 }
-
--- Example/ShipBookSDK/ViewController.swift
+++ Example/ShipBookSDK/ViewController.swift
@@ -3,33 +3,269 @@
 //  ShipBookSDK
 //
 //  Created by Elisha Sterngold on 01/21/2018.
-//  Copyright (c) 2018 hipBook Ltd. All rights reserved.
+//  Copyright (c) 2018 ShipBook Ltd. All rights reserved.
 //
 
 import UIKit
 import ShipBookSDK
 
 fileprivate let log = ShipBook.getLogger(ViewController.self)
+
 class ViewController: UIViewController {
+
+  // MARK: - State
+  private var isUserRegistered = false
+  private var userCounter = 0
+
+  // MARK: - UI Elements
+  private let scrollView = UIScrollView()
+  private let stackView = UIStackView()
+  private var registerButton: UIButton!
+
+  // MARK: - Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
-    log.e("this is an error message")
-    log.w("this is a warning message")
-    log.i("this is an info message")
-    log.d("this is a debug message")
-    log.v("this is a verbose message")
+    title = "ShipBook Example"
+    view.backgroundColor = .systemBackground
+    setupUI()
   }
-  
+
   override func viewDidAppear(_ animated: Bool) {
     super.viewDidAppear(animated)
-    ShipBook.screen(name: "viewController")
-  }
-  
-  override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
+    ShipBook.screen(name: "ViewController")
   }
 
+  // MARK: - UI Setup
+
+  private func setupUI() {
+    scrollView.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(scrollView)
+    NSLayoutConstraint.activate([
+      scrollView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+      scrollView.leadingAnchor.constraint(equalTo: view.leadingAnchor),
+      scrollView.trailingAnchor.constraint(equalTo: view.trailingAnchor),
+      scrollView.bottomAnchor.constraint(equalTo: view.bottomAnchor),
+    ])
+
+    stackView.axis = .vertical
+    stackView.spacing = 12
+    stackView.alignment = .center
+    stackView.translatesAutoresizingMaskIntoConstraints = false
+    scrollView.addSubview(stackView)
+    NSLayoutConstraint.activate([
+      stackView.topAnchor.constraint(equalTo: scrollView.contentLayoutGuide.topAnchor, constant: 20),
+      stackView.bottomAnchor.constraint(equalTo: scrollView.contentLayoutGuide.bottomAnchor, constant: -20),
+      stackView.centerXAnchor.constraint(equalTo: scrollView.centerXAnchor),
+      stackView.widthAnchor.constraint(equalTo: scrollView.widthAnchor, constant: -40),
+    ])
+
+    addUserManagementSection()
+    addScreenTrackingSection()
+    addDivider()
+    addLogLevelsSection()
+    addDivider()
+    addAdvancedFeaturesSection()
+    addDivider()
+    addStressTestSection()
+  }
+
+  // MARK: - Sections
+
+  private func addUserManagementSection() {
+    addSectionLabel("User Management")
+
+    registerButton = makeButton(title: "Register user [test0]", action: #selector(toggleUser))
+    stackView.addArrangedSubview(registerButton)
+  }
+
+  private func addScreenTrackingSection() {
+    addSectionLabel("Screen Tracking")
+
+    let button = makeButton(title: "Set screen to Home", action: #selector(setScreen))
+    stackView.addArrangedSubview(button)
+  }
+
+  private func addLogLevelsSection() {
+    addSectionLabel("Log Levels")
+
+    let row = UIStackView()
+    row.axis = .horizontal
+    row.spacing = 8
+    row.distribution = .fillEqually
+
+    let buttons: [(String, Selector)] = [
+      ("Error", #selector(logError)),
+      ("Warning", #selector(logWarning)),
+      ("Info", #selector(logInfo)),
+      ("Debug", #selector(logDebug)),
+      ("Verbose", #selector(logVerbose)),
+    ]
+    for (title, sel) in buttons {
+      row.addArrangedSubview(makeButton(title: title, action: sel))
+    }
+
+    row.translatesAutoresizingMaskIntoConstraints = false
+    stackView.addArrangedSubview(row)
+    row.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+  }
+
+  private func addAdvancedFeaturesSection() {
+    addSectionLabel("Advanced Features")
+
+    stackView.addArrangedSubview(makeButton(title: "Log Exception", action: #selector(logException)))
+    stackView.addArrangedSubview(makeButton(title: "Throw Uncaught Exception", action: #selector(throwUncaughtException)))
+    stackView.addArrangedSubview(makeButton(title: "Flush Logs", action: #selector(flushLogs)))
+  }
+
+  private func addStressTestSection() {
+    addSectionLabel("Stress Testing")
+
+    stackView.addArrangedSubview(makeButton(title: "Race Condition Test (1000 threads)", action: #selector(raceConditionTest)))
+  }
+
+  // MARK: - Helpers
+
+  private func addSectionLabel(_ text: String) {
+    let label = UILabel()
+    label.text = text
+    label.font = .boldSystemFont(ofSize: 18)
+    label.textAlignment = .center
+    stackView.addArrangedSubview(label)
+  }
+
+  private func addDivider() {
+    let divider = UIView()
+    divider.backgroundColor = .separator
+    divider.translatesAutoresizingMaskIntoConstraints = false
+    divider.heightAnchor.constraint(equalToConstant: 1).isActive = true
+    stackView.addArrangedSubview(divider)
+    divider.widthAnchor.constraint(equalTo: stackView.widthAnchor).isActive = true
+  }
+
+  private func makeButton(title: String, action: Selector) -> UIButton {
+    let button = UIButton(type: .system)
+    button.setTitle(title, for: .normal)
+    button.addTarget(self, action: action, for: .touchUpInside)
+    button.titleLabel?.font = .systemFont(ofSize: 16)
+    return button
+  }
+
+  private func showToast(_ message: String) {
+    let toast = UILabel()
+    toast.text = "  \(message)  "
+    toast.textColor = .white
+    toast.backgroundColor = UIColor.black.withAlphaComponent(0.75)
+    toast.textAlignment = .center
+    toast.font = .systemFont(ofSize: 14)
+    toast.layer.cornerRadius = 8
+    toast.clipsToBounds = true
+    toast.translatesAutoresizingMaskIntoConstraints = false
+    view.addSubview(toast)
+    NSLayoutConstraint.activate([
+      toast.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+      toast.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+      toast.heightAnchor.constraint(greaterThanOrEqualToConstant: 36),
+    ])
+
+    UIView.animate(withDuration: 0.3, delay: 1.0, options: .curveEaseOut, animations: {
+      toast.alpha = 0
+    }, completion: { _ in
+      toast.removeFromSuperview()
+    })
+  }
+
+  // MARK: - Actions
+
+  @objc private func toggleUser() {
+    if isUserRegistered {
+      ShipBook.logout()
+      isUserRegistered = false
+      showToast("Sent: Logout")
+    } else {
+      ShipBook.registerUser(
+        userId: "test\(userCounter)",
+        userName: "testuser\(userCounter)",
+        fullName: "Test User \(userCounter)",
+        email: "test\(userCounter)@test.com",
+        phoneNumber: "+1555000\(userCounter)",
+        additionalInfo: ["role": "tester", "index": "\(userCounter)"]
+      )
+      isUserRegistered = true
+      showToast("Sent: Register user [test\(userCounter)]")
+      userCounter += 1
+    }
+    updateRegisterButtonTitle()
+  }
+
+  private func updateRegisterButtonTitle() {
+    let title = isUserRegistered
+      ? "Logout from Shipbook"
+      : "Register user [test\(userCounter)]"
+    registerButton.setTitle(title, for: .normal)
+  }
+
+  @objc private func setScreen() {
+    ShipBook.screen(name: "Home")
+    showToast("Sent: Set screen to Home")
+  }
+
+  @objc private func logError() {
+    log.e("Test error message")
+    showToast("Sent: Error")
+  }
+
+  @objc private func logWarning() {
+    log.w("Test warning message")
+    showToast("Sent: Warning")
+  }
+
+  @objc private func logInfo() {
+    log.i("Test info message")
+    showToast("Sent: Info")
+  }
+
+  @objc private func logDebug() {
+    log.d("Test debug message")
+    showToast("Sent: Debug")
+  }
+
+  @objc private func logVerbose() {
+    log.v("Test verbose message")
+    showToast("Sent: Verbose")
+  }
+
+  @objc private func logException() {
+    let exception = NSException(
+      name: .genericException,
+      reason: "Test exception from example app",
+      userInfo: nil
+    )
+    log.e("Caught exception: \(exception.name.rawValue) - \(exception.reason ?? "")")
+    showToast("Sent: Log Exception")
+  }
+
+  @objc private func throwUncaughtException() {
+    let numbers: [Int] = []
+    let _ = numbers[0]
+  }
+
+  @objc private func flushLogs() {
+    ShipBook.flush()
+    showToast("Sent: Flush Logs")
+  }
+
+  @objc private func raceConditionTest() {
+    let queue = DispatchQueue(label: "io.shipbook.raceConditionTest", attributes: .concurrent)
+    for i in 1...1000 {
+      queue.async {
+        Log.i("Race condition log #\(i)")
+      }
+    }
+    queue.async(flags: .barrier) {
+      DispatchQueue.main.async { [weak self] in
+        self?.showToast("Race condition test completed")
+      }
+    }
+  }
 }
-
